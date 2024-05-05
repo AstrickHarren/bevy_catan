d36fa03988b86fe2b8ecbfa4d182ae2d2e8db05e
Astrick <astrick.harren.at.work@gmail.com>
2024-05-04 20:12:16 -0400

    feat: add inspector & mod_picking plugins

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ mod load_asset;
 mod map;
 
 use bevy::prelude::*;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use bevy_mod_picking::DefaultPickingPlugins;
 use camera::CameraPlugin;
 use light::LightPlugin;
 use load_asset::LoadAssetPlugin;
@@ -12,9 +14,11 @@ use map::MapPlugin;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugins(WorldInspectorPlugin::new())
         .add_plugins(LoadAssetPlugin)
         .add_plugins(LightPlugin)
         .add_plugins(CameraPlugin)
         .add_plugins(MapPlugin)
+        .add_plugins(DefaultPickingPlugins)
         .run();
 }
